caeaec5d13f0e12997a8ca28fd2b5c61d4f8c8c8
zuoqin <zuoqinr@163.com>
2017-07-13 11:11:24 +0300

    Implementing securities list in Excel sheet

--- src/testblp/core.clj
+++ src/testblp/core.clj
@@ -1,5 +1,15 @@
 (ns testblp.core
-  (:require [clojure.data.json :as json])
+  (:require
+    [clojure.string :as str]
+    [dk.ative.docjure.spreadsheet]
+    [datomic.api :as d]
+
+    [clj-time.core :as t]
+    [clj-time.format :as f]
+    [clj-time.local :as l]
+    [clj-time.coerce :as c]
+  )
+  (use dk.ative.docjure.spreadsheet)
   (:gen-class))
 
 
@@ -14,6 +24,81 @@
 (import com.bloomberglp.blpapi.Service)
 (import com.bloomberglp.blpapi.Session)
 (import java.util.Calendar)
+
+(defn abs [n] (max n (- n)))
+
+(def build-in-basicdate-formatter (f/formatters :basic-date))
+
+(def drive "c")
+
+(def uri "datomic:dev://localhost:4334/sberpb_dev")
+
+(defn ent [id]
+  (let [
+   conn (d/connect uri)
+  ]
+  (seq (d/entity (d/db conn) (ffirst id))) )
+)
+
+(defn update-price [security price]
+  (let [
+    conn (d/connect uri)
+    dt (java.util.Date.)
+    dttxt (f/unparse build-in-basicdate-formatter (c/from-long (c/to-long dt)))
+    secid (ffirst (d/q '[:find ?e
+                        :in $ ?bcode
+                        :where
+                        [?e :security/bcode]
+                        [?s :security/bcode ?bcode]
+                        ]
+                      (d/db conn) security))
+
+
+
+    tr1 (println (str "secid=" secid))
+    priceid (d/q '[:find ?e ;?t ?a ?y ?d ?p ?dur
+             :in $ ?sec
+             :where
+             [?e :price/security ?sec]
+             ] (d/db conn) secid)
+
+    price (ent priceid)
+    target (second (first (filter (fn [x] (if (= (first x) (keyword "price/targetprice")) true false)) price) ))
+
+    anr (second (first (filter (fn [x] (if (= (first x) (keyword "price/analystrating")) true false)) price) ))
+
+    yield (second (first (filter (fn [x] (if (= (first x) (keyword "price/yield")) true false)) price) ))
+
+    dvddate (second (first (filter (fn [x] (if (= (first x) (keyword "price/dvddate")) true false)) price) ))
+
+    putdate (second (first (filter (fn [x] (if (= (first x) (keyword "price/putdate")) true false)) price) ))
+
+    duration (second (first (filter (fn [x] (if (= (first x) (keyword "price/duration")) true false)) price) ))
+
+    tr2 (if (not (nil? priceid)) (d/transact conn [[:db.fn/retractEntity (ffirst priceid)]]))
+
+    ]
+    (d/transact-async conn  [{ :price/security secid :price/lastprice price :price/valuedate dt  :price/source "Bloomberg API import" :price/comment (str "Import from Bllomberg API output on " dttxt) :price/targetprice target :price/analystrating anr :price/yield yield :price/dvddate dvddate :price/putdate putdate :price/duration duration :db/id #db/id[:db.part/user -100001 ]}])
+  )
+
+)
+
+(defn get-price [bcode]
+  (let [
+        conn (d/connect uri)
+        price (ffirst (d/q '[:find ?v
+                        :in $ ?bcode
+                        :where
+                        [?s :security/bcode ?bcode]
+                        [?p :price/lastprice ?v]
+                        [?p :price/security ?s]
+                        ]
+                      (d/db conn) bcode)) 
+
+    ]
+    price
+  )
+)
 
 (defn value-map [f m]
   (zipmap (keys m) (map f (vals m))))
@@ -80,11 +165,42 @@
               (.replace v "\"" ""))))))))
 
 (defn parse-response [response]
+  (let [
+    data (str response)
+    ;tr1 (println (str response))
+    ]
 
-  (loop data (str response) result {}
+    (loop [pos1 0 pos2 0 result {}]
+      (let [
+        newpos1 (str/index-of data "security = \"" pos1)
+        
+        newpos2 (str/index-of data "\n" (+ (if (nil? newpos1) pos1 newpos1) 1))
+        ;tr1 (println (str "pos1=" newpos1 " pos2=" newpos2))
 
+        security (subs data (+ newpos1 12) (- newpos2 3))
+        tr1 (println (str "security=" security) )
 
+        newpos1 (str/index-of data "PX_LAST = " (+ newpos2 10))
+        newpos2 (str/index-of data "\n" (+ (if (nil? newpos1) pos1 newpos1) 1))
+
+        ;;tr1 (println (str "pos1=" newpos1 " pos2=" newpos2))
+
+        ;;pos2 1;;(+ pos1 10)
+        price (subs data (+ newpos1 10) (- newpos2 1))
+
+        prevprice (get-price security)
+
+        tr1 (if (nil? prevprice) (println (str "security=" security " price=" price " prevprice=" prevprice))) 
+
+        ;tr1 (if (> (/ (abs (- (. Double parseDouble price) prevprice)) prevprice) 0.001) (update-price security price))
+
+        ]
+        (if (or (nil? (str/index-of data "security = \"" newpos1)))  result (recur newpos1 newpos2 {}))
+      )    
+    )
   )
+
+  (println "Success")
   ;(println (str "response=" (clojure.string/replace (str response) #"ReferenceDataResponse =" "")))  ;;(first (clojure.string/split (str response) #"^.*PX_LAST.*$"))
   ;; (loop [
   ;;        lines (->> response str clojure.string/split-lines (drop 1) drop-last)
@@ -116,53 +232,65 @@
 (defn request [securities fields & [start-date end-date periodicity]]
   ;periodicity is DAILY WEEKLY MONTHLY QUARTERLY SEMI_ANNUALLY YEARLY
   (locking session-lock
-    (try
-      (let [
-
-      		checksession(if (= @session 0) (reset! session (new-session)))
-            service (.getService @session "//blp/refdata")
-            request-type (if start-date "HistoricalDataRequest" "ReferenceDataRequest")
-            request (.createRequest service request-type)
-            ]
-        (add-items securities "securities")
-        (add-items fields "fields")
-        (when start-date
-          (.set request "startDate" (date->str start-date))
-          (.set request "endDate" (date->str end-date))
-          (.set request "periodicitySelection" periodicity)
-          (.set request "adjustmentSplit" false)
-          (.set request "adjustmentFollowDPDF" false)
-          (.set request "adjustmentNormal" false)
-          (.set request "adjustmentAbnormal" false)
-          )
-        (let [
-              correlation-id (.sendRequest @session request nil)
-              ]
-          (loop [
-                 done []
-                 ]
-            (let [
-                  event (.nextEvent @session)
-                  done (concat done (filter-messages event correlation-id))
-                  ]
-              (if (= Event$EventType/RESPONSE (.eventType event))
-                (map parse-response done)
-                (recur done))))))
-      (catch IllegalStateException e
-        (reset! session (new-session))
-        (request securities fields start-date end-date periodicity)))))
+    (let [
+          checksession(if (= @session 0) (reset! session (new-session)))
+          service (.getService @session "//blp/refdata")
+          request-type (if start-date "HistoricalDataRequest" "ReferenceDataRequest")
+          request (.createRequest service request-type)
+          ] (try
+      
+          (add-items securities "securities")
+          (add-items fields "fields")
+          (when start-date
+            (.set request "startDate" (date->str start-date))
+            (.set request "endDate" (date->str end-date))
+            (.set request "periodicitySelection" periodicity)
+            (.set request "adjustmentSplit" false)
+            (.set request "adjustmentFollowDPDF" false)
+            (.set request "adjustmentNormal" false)
+            (.set request "adjustmentAbnormal" false)
+            )
+          (let [
+                ;;tr1 (println "request1")
+                correlation-id (.sendRequest @session request nil)
+                ;;tr1 (println "request2")
+                ]
+            (loop [
+                   done []
+                   ]
+              (let [
+                    event (.nextEvent @session)
+                    done (concat done (filter-messages event correlation-id))
+                    ]
+                (if (= Event$EventType/RESPONSE (.eventType event))
+                  (map parse-response done)
+                  (recur done)))))
+          (catch Exception e
+            (reset! session (new-session))
+            (request securities fields start-date end-date periodicity))))))
 
 (defn latest-request [securities fields]
   (reduce-map
    (for [
          response (request securities fields)
-         security-data (response "securityData[]")
+         security-data (if (nil? response) [] (response "securityData[]")) 
          ]
      [(security-data "security") (security-data "fieldData")])))
 
 
 
-(def companies (.split (slurp "resources/companies2.csv") "\n"))
+;;(def companies (.split (slurp "resources/companies2.csv") "\n"))
+
+(def companies
+  (let [secs (drop 0 (->> (load-workbook (str drive ":/DEV/clojure/sberpb/sberapi/DB/quotes.xlsx"))
+                   (select-sheet "Data")
+                   (select-columns {:B :bcode})))
+    ]
+    (map (fn [x] (str (:bcode x) "\r") ) secs)
+  )
+)
+
+
 (sort companies)
 
 (def c (take 2 companies))
@@ -173,7 +301,10 @@
                     (map #(-> % first second) c))))
 
 (defn get-descriptions [companies]
-  (value-map get-des (latest-request companies ["PX_LAST"])))
+  ;;(value-map get-des (latest-request companies ["PX_LAST"]))
+  (latest-request companies ["PX_LAST"])
+
+)
 
 
 
